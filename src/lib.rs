adb2edb62fe9965960c45f3e40144c66de8b139a
Serge Radinovich <47865535+sergerad@users.noreply.github.com>
2024-04-10 20:53:01 +1200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/{health}", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    run(listener).await?.await
+}
